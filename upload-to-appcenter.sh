a186ec088ce1156bc5b74ec02933cb8531bdec19
Jun Ma (C) <jun.ma@wiredcraft.com>
2023-10-17 11:29:49 +0800

    add action

---
+++ upload-to-appcenter.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+git config --global --add safe.directory $GITHUB_WORKSPACE
+
+RELEASE_NOTES=""
+isFirst=true
+releaseId=""
+export IFS=";"
+params=()
+[ "${INPUT_NOTIFYTESTERS}" != true ] && params+=(--silent)
+[ "${INPUT_DEBUG}" == true ] && params+=(--debug)
+if [ -n "${INPUT_RELEASENOTES}" ]; then
+    RELEASE_NOTES=${INPUT_RELEASENOTES}
+elif [ $INPUT_GITRELEASENOTES ]; then
+    RELEASE_NOTES="$(git log -1 --pretty=short)"
+fi
+
+if [ -n "${INPUT_BUILDVERSION}" ]; then
+    params+=(--build-version "$INPUT_BUILDVERSION")
+fi
+
+if [ -n "${INPUT_BUILDNUMBER}" ]; then
+    params+=(--build-number "$INPUT_BUILDNUMBER")
+fi
+
+for group in $INPUT_GROUP; do
+    if ${isFirst} ; then
+        isFirst=false
+        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
+        releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | tr -s ' ' | cut -f2 -d ' ' | sort -n -r | head -1)
+    else
+        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
+    fi
+done
